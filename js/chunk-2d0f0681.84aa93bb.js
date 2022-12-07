91ccc8b5911efb8f26ddccaabdbd422603836059
CI <CI@example.com>
2022-12-07 08:13:24 +0000

    deploy

---
+++ js/chunk-2d0f0681.84aa93bb.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f0681"],{"9bdf":function(t,n,e){"use strict";e.r(n);var s=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("v-container",{staticClass:"d-flex justify-center"},[e("div",{staticClass:"main-body"},[t._v(" Contacts ")])])},a=[],c={},i=c,l=e("2877"),u=Object(l["a"])(i,s,a,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0f0681.84aa93bb.js.map
